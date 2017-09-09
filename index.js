ccf23070d6ac08a63b604c8cb7dac86efe3e64dd
Shivam Pathak <pathak.shivambareilly@gmail.com>
2017-09-09 08:36:28 -0700

    Demo

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
-var connect = require('connect'),
-  mongo = require('mongodb');
+var bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
-app.set('port', (process.env.MONGOLAB_URI || 5000));
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
+express()
+  // https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
+  .use(bodyParser.json()) // support json encoded bodies
+  .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
+
+  .get('/api', function (req, res) {
+    res.json(200, {msg: 'OK' });
+  })
+
+  
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
