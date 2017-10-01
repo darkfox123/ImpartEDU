7a41d3289f52449a6d56279feba463d2286f69d4
Shivam Pathak <pathak.shivambareilly@gmail.com>
2017-10-01 03:43:08 -0700

    incorrect1

--- models/class.js
+++ models/class.js
@@ -51,12 +51,12 @@ module.exports.addClass = function(classInst, schoolId, callback){
 // Add classteacher
 module.exports.addClassTeacher = ( teacherId, schoolId ,classname, section, classId, callback) => {
   console.log("got tidin scnd fn : " + teacherId); 
-  Class.findOneAndUpdate({schoolId:schoolId,name:classname,section:section}, {$push: {classTeacher: mongoose.Types.ObjectId(teacherId)}},{new: true},callback
+  Class.findOneAndUpdate({schoolId:schoolId,name:classname,section:section}, {$push: {classTeacher: mongoose.Types.ObjectId(teacherId)}},{new: true},callback);
                         // (err,doc) => {
        //classId = doc._id;
        //console.log("update result doc : " + classId);
        //callback(classId);
-   });
+   //});
 }
 
 module.exports.addResourceToClass = function(classId, resourceId, callback){
