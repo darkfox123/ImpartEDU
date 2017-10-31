8ce406c66d19946a974d7780cf56a3a2bf26cd6d
Shivam Pathak <pathak.shivambareilly@gmail.com>
2017-10-31 14:35:53 -0700

    incorrect1

--- index.js
+++ index.js
@@ -471,10 +471,10 @@ app.post('/api/notifications', function(req, res){
             console.log("notifmap created : " + notifmap);
             Student.addNotifToClass(className, section, city, schoolName,notifmap._id, function(err, students){ if(err){throw err;}
             console.log("updated  : " + count + " : " + counter);
-            //if(counter == count)
-			//{
+            if(counter == count)
+			{
 				console.log("exit here"); res.json(createdNotif);
-				//}
+				}
             counter++;                                                                       
             }); 
         });
