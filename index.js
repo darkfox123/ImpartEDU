39e1a8eaca6e6751759fb98bb3e990d5cb9eebbb
Shivam Pathak <pathak.shivambareilly@gmail.com>
2017-11-03 02:37:22 -0700

    incorrect1

--- index.js
+++ index.js
@@ -630,7 +630,7 @@ app.post('/api/attendance', function(req, res){
         var date = params.date;
         Student.getStudentById(studentId, function(err, studentInst){
             if(err){throw err;}     
-			var attendancePos = JSON.stringify(studentInst.substring(1, 4));
+			var attendancePos = JSON.stringify(studentInst.toString()).substring(1, 4);
 			console.log("attendancePos : " + attendancePos);	
 			
 console.log("student found : " + studentInst);		
