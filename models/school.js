6b0043a47224e168f61c20acf15503f24ad1fc0d
Shivam Pathak <pathak.shivambareilly@gmail.com>
2017-11-26 05:29:50 -0800

    incorrect1

--- models/school.js
+++ models/school.js
@@ -48,7 +48,8 @@ module.exports.addSchools = (school, schoolId, callback) => {
 
 // Add class
 module.exports.addAdmin = function(schoolId ,adminId, callback){ 
-     School.findOneAndUpdate({"schoolId":schoolId}, {"admin":adminId},{multi: true}, function (err, awesome_instance) {
+     console.log("calling school add");
+	 School.findOneAndUpdate({"schoolId":schoolId}, {"admin":adminId},{multi: true}, function (err, awesome_instance) {
   if (err) console.log("err hua");;
         school = awesome_instance;
    console.log("updated scchool : " + school);
