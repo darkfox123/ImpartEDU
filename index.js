858e142dc7f558134e7b10a10cdcde85826b3f54
Shivam Pathak <pathak.shivambareilly@gmail.com>
2018-01-08 12:40:44 -0800

    incorrect1

--- index.js
+++ index.js
@@ -1282,7 +1282,7 @@ Attendance.remove({}, function(err) {
             }
         }
     );
-	
+*/	
 Resource.remove({}, function(err) {
             if (err) {
                 console.log(err)
@@ -1291,7 +1291,7 @@ Resource.remove({}, function(err) {
             }
         }
     );
-	
+/*	
 Application.remove({}, function(err) {
             if (err) {
                 console.log(err)
